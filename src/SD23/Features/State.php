690cfc8954478e0f9e2107d1d536f3e7da480d54
Valdir Lourenco <valdir2310@yahoo.com.br>
2014-11-05 00:57:23 +0000

    state api class

--- src/SD23/Features/State.php
+++ src/SD23/Features/State.php
@@ -15,5 +15,20 @@ class State {
     }
 
 
+    public static function state() {
+        return json_encode(array('status' => 'error'));
+    }
+
+    public static function stateInfo($id) {
+        $app = \Slim\Slim::getInstance();
+        $state = $app->db->state[$id]->fetch();
+        return json_encode($state);
+    }
+
+    public static function cityList($id) {
+        $app = \Slim\Slim::getInstance();
+        $countryList = $app->db->city(array("state_id" => $id))->fetchAll();
+        return json_encode($countryList);
+    }
 
 }
--- src/routes.php
+++ src/routes.php
@@ -15,9 +15,15 @@ $app->get('/country/:id/state', function ($id) {
     echo \SD23\Features\Country::stateList($id);
 });
 
-$app->get('/state', function () {});
-$app->get('/state/:id', function ($id) {});
-$app->get('/state/:id/city', function ($id) {});
+$app->get('/state', function () {
+    echo \SD23\Features\State::state();
+});
+$app->get('/state/:id', function ($id) {
+    echo \SD23\Features\State::stateInfo($id);
+});
+$app->get('/state/:id/city', function ($id) {
+    echo \SD23\Features\State::cityList($id);
+});
 
 $app->get('/city', function () {});
 $app->get('/city/:id', function ($id) {});
